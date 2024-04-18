2cd9bf5437a9b2bf20b40b7089a9f1e6ecfb3595
Salomão Ribeiro <salomaomdm@gmail.com>
2024-04-18 13:43:53 -0300

    Docker Volumes

---
+++ Docker Swarm - Kubernates/2_volumes/process.php
@@ -0,0 +1,13 @@
+<?php
+$message = $_POST['message'];
+$files = scandir('./messages');
+$num_files = count($files) -2; // remover o . e o ..
+
+$filename = "msg-{$num_files}.txt";
+
+$file = fopen("./messages/{$filename}", "x");
+
+fwrite($file, $message);
+fclose($file);
+
+header("Location: index.php");
